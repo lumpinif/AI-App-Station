5e0f58a602f2e70124007e3d8f369fca6fa47e11
Felix <pengfeilyu1018@gmail.com>
2024-01-05 22:10:14 +0800

    refactor the home screen and building sign in page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/home/HeroSection"
 export default function IntroductionPage() {
   return (
     <>
-      <div className="h-[2000px] ">
+      <div className="h-[2000px]">
         <section className="container grid items-center gap-6 pb-8 sm:pt-6 md:py-10 ">
           <HeroSection />
         </section>
--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -6,7 +6,7 @@ import { TextTypingEffectWithTextsFadeOut } from "./TypingEffect"
 const HeroSection = () => {
   return (
     <>
-      <div className="relative mt-16 flex h-[200px] select-none items-center justify-center text-center text-4xl font-extrabold leading-tight tracking-tighter sm:h-[300px] md:text-5xl lg:text-6xl xl:text-7xl">
+      <div className="relative mt-16 flex h-[150px] select-none items-center justify-center text-center text-4xl font-extrabold leading-tight tracking-tighter sm:h-[200px] md:h-[300px] md:text-5xl lg:text-6xl xl:text-7xl">
         <Link
           href="/today"
           className="ease absolute top-[-30px] mx-auto justify-center rounded-full px-10 py-2 text-center text-base font-medium leading-none tracking-normal text-muted-foreground transition-all duration-300 hover:scale-105 sm:top-3 sm:text-lg md:text-xl"
@@ -17,22 +17,35 @@ const HeroSection = () => {
         <TextTypingEffectWithTextsFadeOut />
       </div>
 
-      <div className="flex min-w-[350px] items-center justify-center gap-4 text-center text-xl drop-shadow-sm lg:min-w-full">
-        <Link href="/today">
-          <Button className="hover:shadow-inner-outline">Get Started</Button>
-        </Link>
-      </div>
-
-      <div className="mt-16 flex min-w-[350px] flex-col items-center justify-center gap-4 text-center text-xl drop-shadow-sm lg:min-w-full">
+      <div className="mt-4 flex min-w-[350px] flex-col items-center justify-center gap-4 text-center text-xl drop-shadow-sm sm:mt-16 lg:min-w-full">
         <div className="max-w-md font-semibold sm:max-w-fit md:text-2xl lg:text-3xl xl:text-4xl">
           Probably the last AI Apps & News Station you&apos;ll ever need.
         </div>{" "}
-        <div className="max-w-md text-base text-muted-foreground  sm:max-w-xl sm:text-lg md:max-w-2xl md:text-xl lg:max-w-4xl">
+        <div className="max-w-md text-xs  text-muted-foreground sm:max-w-xl sm:text-lg md:max-w-2xl md:text-xl lg:max-w-4xl">
           Stay ahead of the curve with cutting-edge solutions in the age of AI
           with Today&apos;s news, the newest AI Apps with collections,
           categories, and stories completely for{" "}
           <span className="font-bold uppercase text-foreground">free</span>.
         </div>
+      </div>
+
+      <div className="mt-3 flex min-w-[350px] items-center justify-center gap-4 text-center text-xl drop-shadow-sm sm:mt-20 lg:min-w-full">
+        <Link href="/today">
+          <Button className="w-28 hover:shadow-inner-outline">
+            Get Started
+          </Button>
+        </Link>
+        <span className="flex text-xs text-muted-foreground sm:text-sm">
+          or
+        </span>
+        <Link href="/" className="sm:flex">
+          <Button
+            className="w-28 hover:shadow-inner-outline"
+            variant={"outline"}
+          >
+            Sign In
+          </Button>
+        </Link>
       </div>
     </>
   )
