140904fd5e7e50b0e093165bb826d3f24934bc63
Felix <pengfeilyu1018@gmail.com>
2024-01-31 00:39:26 +0800

    build: app submit page ui

--- app/(main)/ai-apps/submit/page.tsx
+++ app/(main)/ai-apps/submit/page.tsx
@@ -1,5 +1,96 @@
-const SubmitPage = () => {
-  return <div>Submit Page</div>
+"use client"
+
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+})
+
+const AppSubmitPage = () => {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  })
+
+  const { isSubmitting, isValid } = form.formState
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      toast.success(`${values.title} - App Submited`)
+    } catch {
+      toast.error("Something went wrong")
+    }
+  }
+
+  return (
+    <div className="mx-auto flex h-screen max-w-7xl items-center justify-center p-6">
+      <div>
+        <h1 className="text-2xl font-medium">Submit the AI App</h1>
+        <p className="text-sm text-foreground">
+          What is the name of this AI App ?
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-8 space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course title</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'ChatGPT'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Don&apos;t worry, you can change this later.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/ai-apps">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
-export default SubmitPage
+export default AppSubmitPage
--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -31,7 +31,7 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none opacity-0 transition-colors",
+      "flex touch-none select-none transition-colors",
       orientation === "vertical" &&
         "h-full w-2.5 border-l border-l-transparent p-[1px]",
       orientation === "horizontal" &&
@@ -40,7 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-muted dark:bg-muted-foreground" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
