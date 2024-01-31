3c26daa7d3c73774618006c6ec81e4116831e0b7
Felix <pengfeilyu1018@gmail.com>
2024-02-01 00:12:02 +0800

    fix: scroll bar in the scroll area to py-4

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -33,7 +33,7 @@ const ScrollBar = React.forwardRef<
     className={cn(
       "flex touch-none select-none transition-colors",
       orientation === "vertical" &&
-        "h-full w-1.5 border-l border-l-transparent p-[1px] py-3",
+        "h-full w-1.5 border-l border-l-transparent p-[1px] py-4",
       orientation === "horizontal" &&
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
       className
