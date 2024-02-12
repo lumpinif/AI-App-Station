27f9cf0ca5555937442e9fba8ef8cc09754525a9
Felix <pengfeilyu1018@gmail.com>
2024-02-13 02:14:21 +0800

    fix: Update loading.tsx with h-screen ...etc

--- app/(auth)/loading.tsx
+++ app/(auth)/loading.tsx
@@ -2,7 +2,7 @@ import { LoadingSpinner } from "@/components/layout/loading-spinner"
 
 export default function Loading() {
   return (
-    <div className="h-full">
+    <div className="flex h-screen w-screen items-center justify-center">
       <LoadingSpinner />
     </div>
   )
