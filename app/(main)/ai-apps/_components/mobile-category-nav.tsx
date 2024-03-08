7430f80586d67b18ac453cc870d76fbf0444d258
Felix <pengfeilyu1018@gmail.com>
2024-03-08 21:00:19 +0800

    refactor: add chevrons to the category tags

--- app/(main)/ai-apps/_components/mobile-category-nav.tsx
+++ app/(main)/ai-apps/_components/mobile-category-nav.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname, useRouter } from "next/navigation"
 import { motion } from "framer-motion"
+import { ChevronDown, ChevronRight } from "lucide-react"
 
 import { SIDENAVROUTES } from "@/config/routes"
 import { cn } from "@/lib/utils"
@@ -20,14 +21,13 @@ export function MobileCategoryNav() {
   return (
     <div className="sm:hidden">
       <div className="flex flex-col">
-        <div className="scrollbar-none flex gap-x-6 overflow-x-auto py-1 sm:hidden">
+        <div className="scrollbar-none flex items-center gap-x-6 overflow-x-auto py-1 sm:hidden">
           {SIDENAVROUTES.map((route) => (
             <div
               key={route.title}
               onClick={() => {
                 handleTabSelection(`${route.href}`)
               }}
-              // href={`${route.href}`}
               className={cn(
                 "relative flex cursor-pointer items-center gap-x-2 whitespace-nowrap transition-colors ease-out focus-visible:ring-4 focus-visible:ring-ring",
                 pathname.includes(`${route.href}`)
@@ -38,19 +38,24 @@ export function MobileCategoryNav() {
                 WebkitTapHighlightColor: "transparent",
               }}
             >
-              {pathname.includes(`${route.href}`) && (
-                <motion.span
-                  layoutId="bubble_underline"
-                  className="absolute inset-0 z-10 border-b-2 border-primary pb-1"
-                  transition={{
-                    type: "spring",
-                    bounce: 0.2,
-                    duration: 0.35,
-                    ease: [0.32, 0.72, 0, 1],
-                  }}
-                />
-              )}
               <span>{route.title}</span>
+              {pathname.includes(`${route.href}`) ? (
+                <>
+                  <motion.span
+                    layoutId="bubble_underline"
+                    className="absolute inset-0 z-10 border-b-2 border-primary pb-1"
+                    transition={{
+                      type: "spring",
+                      bounce: 0.2,
+                      duration: 0.35,
+                      ease: [0.32, 0.72, 0, 1],
+                    }}
+                  />
+                  <ChevronDown className="h-4 w-4 -translate-x-1" />
+                </>
+              ) : (
+                <ChevronRight className="h-4 w-4 -translate-x-1" />
+              )}
             </div>
           ))}
         </div>
