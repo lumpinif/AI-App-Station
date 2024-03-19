e5b9848af05bc10765f23cc155befa68ba82b5cb
Felix Lu <pengfeilyu1018@gmail.com>
2024-03-20 00:11:40 +0800

    refactor: fix style of site-footer

--- components/layout/site-footer/site-footer.tsx
+++ components/layout/site-footer/site-footer.tsx
@@ -5,23 +5,25 @@ import { ThemeToggle } from "@/components/theme/theme-toggle"
 
 export function SiteFooter() {
   return (
-    <footer className="container mt-2 flex h-24 items-center justify-between border-t-[0.5px]">
-      <div className="flex w-full flex-1 flex-nowrap gap-4 sm:justify-start">
-        <span className="flex-nowrap gap-1 text-balance text-left text-xs leading-loose text-muted-foreground">
-          © 2024 OpenmindAI.io All rights reserved. Built by{" "}
-          <Link
-            href={siteConfig.links.twitter}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium"
-          >
-            Felix Lu.
-          </Link>{" "}
-        </span>
-      </div>
-      <div className="hidden sm:flex">
-        <ThemeToggle />
-      </div>
-    </footer>
+    <section className="container">
+      <footer className="mt-2 flex h-24 items-center justify-between border-t-[0.5px] px-1">
+        <div className="flex w-full flex-1 flex-nowrap gap-4 sm:justify-start">
+          <span className="flex-nowrap gap-1 text-balance text-left text-xs leading-loose text-muted-foreground">
+            © 2024 OpenmindAI.io All rights reserved. Built by{" "}
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium"
+            >
+              Felix Lu.
+            </Link>{" "}
+          </span>
+        </div>
+        <div className="hidden sm:flex">
+          <ThemeToggle />
+        </div>
+      </footer>
+    </section>
   )
 }
