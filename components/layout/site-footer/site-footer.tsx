59b53f551261c517aa2e987c74cf765b1bf96e0b
Felix Lu <pengfeilyu1018@gmail.com>
2024-03-20 00:16:46 +0800

    refactor: fix the style of the site-footer

--- components/layout/site-footer/site-footer.tsx
+++ components/layout/site-footer/site-footer.tsx
@@ -6,9 +6,9 @@ import { ThemeToggle } from "@/components/theme/theme-toggle"
 export function SiteFooter() {
   return (
     <section className="container">
-      <footer className="mt-2 flex h-24 items-center justify-between border-t-[0.5px] px-1">
-        <div className="flex w-full flex-1 flex-nowrap gap-4 sm:justify-start">
-          <span className="flex-nowrap gap-1 text-balance text-left text-xs leading-loose text-muted-foreground">
+      <footer className="mt-2 flex h-24 items-center justify-between border-t-[0.5px] ">
+        <div className="flex w-full flex-1 flex-nowrap justify-center gap-4 sm:justify-start">
+          <span className="flex-nowrap gap-1 text-balance text-center text-xs leading-loose text-muted-foreground sm:text-left">
             © 2024 OpenmindAI.io All rights reserved. Built by{" "}
             <Link
               href={siteConfig.links.twitter}
