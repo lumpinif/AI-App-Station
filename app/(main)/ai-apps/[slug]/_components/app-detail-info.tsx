24a250536c4e653fe099926a9a9b7b2d8bbd7dda
Felix Lu <pengfeilyu1018@gmail.com>
2024-04-14 01:25:04 +0800

    fix: hydration error for app-detail-infor Categories

--- app/(main)/ai-apps/[slug]/_components/app-detail-info.tsx
+++ app/(main)/ai-apps/[slug]/_components/app-detail-info.tsx
@@ -1,8 +1,9 @@
+import { Fragment } from "react"
 import Link from "next/link"
 import { Rating } from "@mui/material"
 import { Star } from "lucide-react"
 
-import { AppDetails, Categories } from "@/types/db_tables"
+import { AppDetails, Categories as CategoriesProps } from "@/types/db_tables"
 import { cn } from "@/lib/utils"
 import { Separator } from "@/components/ui/separator"
 
@@ -22,60 +23,34 @@ export const AppDetailInfo: React.FC<AppDetailInfoProps> = ({
   rating_count,
 }) => {
   return (
-    <>
-      <div
-        className={cn(
-          "no-scrollbar w-full max-w-full snap-x overflow-x-auto",
-          className
-        )}
-      >
-        <div className="flex h-20 w-full min-w-max shrink-0 flex-row items-center max-sm:justify-between">
-          <div className="flex h-full w-28 shrink-0 items-center justify-center overflow-hidden sm:w-32 md:w-40 lg:w-44">
-            <RatingsAndReviews
-              rating_count={rating_count}
-              rating_score={rating_score}
-            />
-          </div>
-          <Separator orientation="vertical" className="h-1/2" />
-          {app.categories && app.categories.length > 0 ? (
-            app.categories?.map((category, index) => (
-              <div key={index}>
-                <div
-                  key={category.category_id}
-                  className="flex h-full w-28 shrink-0 items-center justify-center sm:w-32 md:w-40 lg:w-44"
-                >
-                  <Category
-                    category_slug={category.slug}
-                    category_title={category.category_title}
-                    className="underline-offset-4 hover:font-medium hover:underline"
-                  />
-                </div>
-                <Separator orientation="vertical" className="h-1/2" />
-              </div>
-            ))
-          ) : (
-            <div>
-              <div className="flex h-full w-28 shrink-0 items-center justify-center sm:w-32 md:w-40 lg:w-44">
-                <span className="text-xs">No Category</span>
-              </div>
-              <Separator orientation="vertical" className="h-1/2" />
-            </div>
-          )}
-
-          <div className="flex h-full w-28 shrink-0 items-center justify-center sm:w-32 md:w-40 lg:w-44">
-            <AppCommentsBadge
-              app_slug={app.app_slug}
-              comments_count={app.comments_count}
-              className="underline-offset-4 hover:font-medium"
-            />
-          </div>
-          <Separator orientation="vertical" className="h-1/2" />
-          <div className="lg:w-44shrink-0 flex h-full w-28 items-center justify-center sm:w-32 md:w-40">
-            <Pricing data={app} className="w-full text-center" />
-          </div>
+    <div
+      className={cn(
+        "no-scrollbar w-full max-w-full snap-x overflow-x-auto",
+        className
+      )}
+    >
+      <div className="flex h-20 w-full min-w-max shrink-0 flex-row items-center max-sm:justify-between">
+        <div className="flex h-full w-28 shrink-0 items-center justify-center overflow-hidden sm:w-32 md:w-40 lg:w-44">
+          <RatingsAndReviews
+            rating_count={rating_count}
+            rating_score={rating_score}
+          />
+        </div>
+        <Separator orientation="vertical" className="h-1/2" />
+        <Categories data={app} />
+        <div className="flex h-full w-28 shrink-0 items-center justify-center sm:w-32 md:w-40 lg:w-44">
+          <AppCommentsBadge
+            app_slug={app.app_slug}
+            comments_count={app.comments_count}
+            className="underline-offset-4 hover:font-medium"
+          />
+        </div>
+        <Separator orientation="vertical" className="h-1/2" />
+        <div className="lg:w-44shrink-0 flex h-full w-28 items-center justify-center sm:w-32 md:w-40">
+          <Pricing data={app} className="w-full text-center" />
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
@@ -120,18 +95,47 @@ const Category = ({
   category_slug,
   className,
 }: {
-  category_title: Categories["category_title"]
-  category_slug: Categories["slug"]
+  category_title: CategoriesProps["category_title"]
+  category_slug: CategoriesProps["slug"]
   className?: string
 }) => {
   return (
-    <>
-      <Link
-        href={`/ai-apps/categories/${category_slug}`}
-        className={cn(className)}
-      >
-        <span className="text-xs">{category_title}</span>
-      </Link>
-    </>
+    <Link
+      href={`/ai-apps/categories/${category_slug}`}
+      className={cn(className)}
+    >
+      <span className="text-xs">{category_title}</span>
+    </Link>
+  )
+}
+
+const Categories = ({ data: app }: { data: AppDetails }) => {
+  return (
+    <Fragment>
+      {app.categories && app.categories.length > 0 ? (
+        app.categories?.map((category, index) => (
+          <Fragment key={index}>
+            <div
+              key={category.category_id}
+              className="flex h-full w-28 shrink-0 items-center justify-center sm:w-32 md:w-40 lg:w-44"
+            >
+              <Category
+                category_slug={category.slug}
+                category_title={category.category_title}
+                className="underline-offset-4 hover:font-medium hover:underline"
+              />
+            </div>
+            <Separator orientation="vertical" className="h-1/2" />
+          </Fragment>
+        ))
+      ) : (
+        <Fragment>
+          <div className="flex h-full w-28 shrink-0 items-center justify-center sm:w-32 md:w-40 lg:w-44">
+            <span className="text-xs">No Category</span>
+          </div>
+          <Separator orientation="vertical" className="h-1/2" />
+        </Fragment>
+      )}
+    </Fragment>
   )
 }
