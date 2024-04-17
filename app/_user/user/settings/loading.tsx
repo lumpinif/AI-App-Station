9c2055df460356401bd9dcec823be63b1f9a603a
Felix Lu <pengfeilyu1018@gmail.com>
2024-04-17 22:06:50 +0800

    remove: unused pre-built components

--- app/_user/user/settings/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from "@/components/layout/loading-spinner"
-
-export default function Loading() {
-  return (
-    <div className="h-full">
-      <LoadingSpinner />
-    </div>
-  )
-}
