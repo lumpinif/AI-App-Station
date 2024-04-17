342350fa12e3b7dd155563424f6c837093f0eb58
Felix Lu <pengfeilyu1018@gmail.com>
2024-04-17 18:54:16 +0800

    fix: remove duplicated accout modal trigger on the landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,9 @@
 import { UnderlayActionSheet } from "@/components/ui/underlay-action-sheet"
-import AccountModalTrigger from "@/components/auth/auth-modal/account-modal-trigger"
 import HeroSection from "@/components/landing/HeroSection"
 
 export default function LandingPage() {
   return (
     <>
-      <div className="absolute right-4 top-3 sm:hidden">
-        <AccountModalTrigger />
-      </div>
       <div className="flex flex-col">
         <main className="min-h-dvh flex-1">
           <HeroSection />
