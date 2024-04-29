8431b6e59091d1240993d667c111a1fa01bbeb6f
Felix Lu <pengfeilyu1018@gmail.com>
2024-04-29 12:00:16 +0800

    ADD: react-copy-to-clipboard

--- components/shared/copy-button.tsx
+++ components/shared/copy-button.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { usePathname } from "next/navigation"
 import { AnimatePresence, motion } from "framer-motion"
 import { Check, Copy } from "lucide-react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 import { toast } from "sonner"
 
 import { App } from "@/types/db_tables"
@@ -38,16 +39,13 @@ export const CopyButton: React.FC<CopyButtonProps> = ({ className }) => {
     `${getSiteUrl()}${pathname}` || `${window.location.origin}${pathname}`
 
   return (
-    <Button
-      size={"sm"}
-      className={cn("max-w-sm", className)}
-      disabled={buttonState === "loading"}
-      onClick={() => {
+    <CopyToClipboard
+      text={url}
+      onCopy={() => {
         if (buttonState === "success") return
 
-        setTimeout(() => {
+        setTimeout(async () => {
           setButtonState("success")
-          navigator.clipboard.writeText(url)
           toast.success("Copied to clipboard", { description: url })
         }, 0)
 
@@ -56,18 +54,24 @@ export const CopyButton: React.FC<CopyButtonProps> = ({ className }) => {
         }, 3500)
       }}
     >
-      <AnimatePresence mode="popLayout" initial={false}>
-        <motion.span
-          transition={{ type: "spring", duration: 0.3, bounce: 0 }}
-          initial={{ opacity: 0, y: 25 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -25 }}
-          key={buttonState}
-          className="flex w-fit items-center space-x-2 text-nowrap"
-        >
-          {buttonCopy[buttonState]}
-        </motion.span>
-      </AnimatePresence>
-    </Button>
+      <Button
+        size={"sm"}
+        className={cn("max-w-sm", className)}
+        disabled={buttonState === "loading"}
+      >
+        <AnimatePresence mode="popLayout" initial={false}>
+          <motion.span
+            transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+            initial={{ opacity: 0, y: 25 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -25 }}
+            key={buttonState}
+            className="flex w-fit items-center space-x-2 text-nowrap"
+          >
+            {buttonCopy[buttonState]}
+          </motion.span>
+        </AnimatePresence>
+      </Button>
+    </CopyToClipboard>
   )
 }
