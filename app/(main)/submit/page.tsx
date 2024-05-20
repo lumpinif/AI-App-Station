5f11a7c5253e0c9b518b424fccd3223deb1fca6c
Felix Lu <pengfeilyu1018@gmail.com>
2024-05-21 02:15:08 +0800

    fix: removed incorrect useage of props of className in the page.tsx

--- app/(main)/submit/page.tsx
+++ app/(main)/submit/page.tsx
@@ -1,15 +1,8 @@
-import { cn } from "@/lib/utils"
-
 import AppSubmitForm from "./_components/app-submit-form"
 
-const AppSubmitPage = ({ className }: { className?: string }) => {
+const AppSubmitPage = () => {
   return (
-    <div
-      className={cn(
-        "mx-auto flex max-w-2xl items-center justify-center p-6",
-        className
-      )}
-    >
+    <div className="mx-auto flex max-w-2xl items-center justify-center p-6">
       <div className="h-full w-full">
         <h1 className="text-2xl font-medium">Submit the AI App</h1>
         <p className="text-muted-foreground mt-1 text-sm">
