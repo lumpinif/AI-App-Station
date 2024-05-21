71a5d40359010907573efa7581efdab89a080576
Felix Lu <pengfeilyu1018@gmail.com>
2024-05-22 00:25:12 +0800

    refactor: add BreadcrumbPage component for editor route add isEditorRoute to check if it is the editor and conditionally render the editor BreadcrumbItem
    styles: conditionally make the BreadcrumbLink text primary if the page is at its page

--- app/(main)/user/_components/layout/user-pages-breadcrumb.tsx
+++ app/(main)/user/_components/layout/user-pages-breadcrumb.tsx
@@ -12,14 +12,20 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
+  BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 
 import { UserPagesMobileNavSheet } from "./user-pages-mobile-nav-sheet"
 
-type UserBreadCrumbProps = {}
+const isEditorRoute = (pathname: string) => {
+  // Regular expression pattern to match a UUID after '/stories/' or '/apps/'
+  const uuidPattern =
+    /\/(stories|apps)\/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/
+  return uuidPattern.test(pathname)
+}
 
-export const UserPagesBreadCrumb: React.FC<UserBreadCrumbProps> = () => {
+export const UserPagesBreadCrumb = () => {
   const pathname = usePathname()
 
   // Find the matching route based on the current pathname
@@ -60,8 +66,23 @@ export const UserPagesBreadCrumb: React.FC<UserBreadCrumbProps> = () => {
                 <BreadcrumbSeparator />
                 <BreadcrumbItem>
                   <BreadcrumbLink asChild>
-                    <Link href={items[0].href}>{items[0].title}</Link>
+                    <Link
+                      href={items[0].href}
+                      className={
+                        pathname === items[0].href ? "text-primary" : ""
+                      }
+                    >
+                      {items[0].title}
+                    </Link>
                   </BreadcrumbLink>
+                </BreadcrumbItem>
+              </>
+            )}
+            {isEditorRoute(pathname) && (
+              <>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Editor</BreadcrumbPage>
                 </BreadcrumbItem>
               </>
             )}
