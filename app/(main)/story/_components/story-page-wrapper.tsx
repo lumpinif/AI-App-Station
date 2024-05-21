8b7cf746854163f9f93be73096caa5679af7f614
Felix Lu <pengfeilyu1018@gmail.com>
2024-05-21 23:50:20 +0800

    styles: remove the shadow of the story page wrapper

--- app/(main)/story/_components/story-page-wrapper.tsx
+++ app/(main)/story/_components/story-page-wrapper.tsx
@@ -4,7 +4,7 @@ type StoryPageWrapperProps = {
 
 const StoryPageWrapper: React.FC<StoryPageWrapperProps> = ({ children }) => {
   return (
-    <main className="mx-auto w-full max-w-4xl space-y-4 rounded-lg shadow-sm sm:space-y-6 sm:px-6 sm:py-4 md:space-y-8">
+    <main className="mx-auto w-full max-w-4xl space-y-4 rounded-lg sm:space-y-6 sm:px-6 sm:py-4 md:space-y-8">
       {children}
     </main>
   )
