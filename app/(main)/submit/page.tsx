6a302e303e5cbde83421dbb28e8ccbfa9e0bdfdc
Felix Lu <pengfeilyu1018@gmail.com>
2024-05-22 19:46:22 +0800

    refactor: update page title and text styles in submit page
    refactor: diasble ReactQueryDevtools for now

--- app/(main)/submit/page.tsx
+++ app/(main)/submit/page.tsx
@@ -4,9 +4,11 @@ const AppSubmitPage = () => {
   return (
     <div className="mx-auto flex max-w-2xl items-center justify-center p-6">
       <div className="h-full w-full">
-        <h1 className="text-2xl font-medium">Submit the AI App</h1>
-        <p className="text-muted-foreground mt-1 text-sm">
-          Help us to expand our knowledge for the whole humanity.
+        <h1 className="text-2xl font-bold tracking-[-.016em] md:text-3xl md:tracking-[-.024em]">
+          Submit the AI App
+        </h1>
+        <p className="text-muted-foreground mt-1 text-sm md:text-base">
+          Help us to expand our knowledge for the whole world.
         </p>
         <AppSubmitForm />
       </div>
