c1b15904fee783f10d5cde3e1eae8f91015ebd29
Felix Lu <pengfeilyu1018@gmail.com>
2024-05-25 00:55:58 +0800

    refactor: Update PopoverMenu component styles and animations

--- components/ui/popover-menu.tsx
+++ components/ui/popover-menu.tsx
@@ -134,7 +134,7 @@ export default function PopoverMenu({
       </AnimatePresence>
       <motion.button
         className={cn(
-          "text-muted-foreground bg-background absolute bottom-0 left-4 flex h-12 w-12 items-center justify-center rounded-full p-2 outline-none",
+          "text-muted-foreground hover:!bg-foreground/10  hover:text-foreground absolute bottom-0 left-4 flex h-12 w-12 items-center justify-center rounded-full p-2 outline-none",
           buttonClassName
         )}
         disabled={isPopoverOpen}
