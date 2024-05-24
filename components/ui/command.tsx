fc4b399748f73ff0ab7f3e56b328ae52989537d8
Felix Lu <pengfeilyu1018@gmail.com>
2024-05-24 23:05:46 +0800

    styles: update CommandPrimitive.Item styles

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -131,7 +131,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "aria-selected:bg-accent/50 aria-selected:text-accent-foreground relative flex cursor-default select-none items-center rounded-lg px-2 py-1.5 text-sm outline-none active:scale-[0.98] data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50",
+      "aria-selected:bg-accent dark:aria-selected:bg-accent/50 aria-selected:text-accent-foreground relative flex cursor-default select-none items-center rounded-lg px-2 py-1.5 text-sm outline-none active:scale-[0.98] data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50",
       className
     )}
     {...props}
