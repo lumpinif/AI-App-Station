0a82b3bcbf387a656b62ba574cfe596ab3950cb8
Felix Lu <pengfeilyu1018@gmail.com>
2024-06-04 17:58:04 +0800

    refactor: add new story button to the new page of the story route

--- app/(main)/story/new/page.tsx
+++ app/(main)/story/new/page.tsx
@@ -1,10 +1,22 @@
 import StoryContentWrapper from "../_components/story-content/story-content-wrapper"
+import { WriteNewStoryButton } from "../_components/story-content/write-new-story-button"
 
 export default function StoryPage() {
   return (
     <main>
       <StoryContentWrapper>
-        <div className="">Show the post submit button</div>
+        <div className="flex h-[calc(100vh-30rem)] w-full items-center justify-center">
+          <div className="flex flex-col items-center gap-y-6">
+            <span className="page-title-font text-2xl">
+              Click the button to Write a new story
+            </span>
+            <WriteNewStoryButton
+              variant={"default"}
+              size={"default"}
+              spinnerButtonCN="w-32 rounded-full border dark:border-0 dark:shadow-outline"
+            />
+          </div>
+        </div>
       </StoryContentWrapper>
     </main>
   )
