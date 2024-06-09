673a0fa522f6b7fb39ca081ede1b6d927bac8736
Felix Lu <pengfeilyu1018@gmail.com>
2024-06-09 22:08:44 +0800

    refactor: show the search results first in the search command dialog
    styles: updaetd the search command dialog styles

--- components/search-command-dialog/search-command-dialog.tsx
+++ components/search-command-dialog/search-command-dialog.tsx
@@ -71,9 +71,9 @@ export const SearchCommandDialog: React.FC<SearchCommandDialogProps> = ({
       open={isOpen}
       onOpenChange={toggleSearchDialog}
       commandClassName="bg-background focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 [&_[cmdk-group-heading]]:text-primary dark:[&_[cmdk-group-heading]]:text-muted-foreground "
-      className="h-[calc(100vh-3rem)] max-w-full rounded-2xl p-4 data-[state=closed]:max-md:slide-out-to-bottom-5 data-[state=open]:max-md:!slide-in-from-bottom-5 sm:max-w-lg sm:rounded-3xl md:top-1/3 md:h-[720px] md:max-w-xl lg:max-w-2xl xl:max-w-3xl"
+      className="h-[calc(100vh-5rem)] max-w-full rounded-2xl p-4 data-[state=closed]:max-md:slide-out-to-bottom-5 data-[state=open]:max-md:!slide-in-from-bottom-5 sm:max-w-lg sm:rounded-3xl md:top-1/3 md:h-[720px] md:max-w-xl lg:max-w-2xl xl:max-w-3xl"
     >
-      <div className="mb-2 border-b p-3 transition-all duration-150 ease-out [&_[cmdk-input-wrapper]]:rounded-full [&_[cmdk-input-wrapper]]:border-none focus-within:[&_[cmdk-input-wrapper]]:bg-input">
+      <div className="mb-2 p-3 transition-all duration-150 ease-out [&_[cmdk-input-wrapper]]:rounded-full [&_[cmdk-input-wrapper]]:border-none focus-within:[&_[cmdk-input-wrapper]]:bg-input">
         <CommandInput
           value={search}
           placeholder="Type a command or search..."
@@ -86,8 +86,6 @@ export const SearchCommandDialog: React.FC<SearchCommandDialogProps> = ({
           <CommandList className="relative size-full !max-h-none pr-2 md:px-4">
             <CommandEmpty>No results found.</CommandEmpty>
 
-            <SearchCommandGroups runCommand={runCommand} />
-
             {search && db_apps && (
               <AppsCommandGroup db_apps={db_apps} runCommand={runCommand} />
             )}
@@ -98,6 +96,8 @@ export const SearchCommandDialog: React.FC<SearchCommandDialogProps> = ({
                 runCommand={runCommand}
               />
             )}
+
+            <SearchCommandGroups runCommand={runCommand} />
           </CommandList>
         </ScrollArea>
       </section>
