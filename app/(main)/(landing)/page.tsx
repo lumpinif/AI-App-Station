1c10f1535deeb61028f2b6481b26cdc9b9e5857d
Felix Lu <pengfeilyu1018@gmail.com>
2024-08-01 17:44:52 +0800

    refactor: redirect every user from landing page to if it is not super user

--- app/(main)/(landing)/page.tsx
+++ app/(main)/(landing)/page.tsx
@@ -1,11 +1,20 @@
-import { getUser } from "@/server/auth"
+import { redirect } from "next/navigation"
+import { getUser, getUserProfile } from "@/server/auth"
 
+import { checkIsSuperUser } from "@/lib/utils"
 import AiAppsPageIntro from "@/components/landing/ai-apps-page-intro"
 import HeroSection from "@/components/landing/magic-ui/hero-section"
 import TodayPageIntro from "@/components/landing/today-page-intro"
 
 export default async function LandingPage() {
   const { user } = await getUser()
+  const { profile } = await getUserProfile()
+  const isSuperUser = checkIsSuperUser(profile?.profile_role?.role)
+
+  // redirect to /ai-apps if it is not super user
+  if (!isSuperUser) {
+    redirect("/ai-apps")
+  }
 
   return (
     <main className="mx-auto flex min-h-dvh animate-magic-fade-in flex-col space-y-20 sm:space-y-44">
